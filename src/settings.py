cfbd83b5eaca15041d28fa63d8c35e42af6d3325
Abhijeet Gupta <abhijeetg40@gmail.com>
2021-04-18 20:15:55 +0530

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -125,6 +125,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'abhijeetg40@gmail.com'
-EMAIL_HOST_PASSWORD = 'golumolujeetu'
+EMAIL_HOST_USER = 'email'
+EMAIL_HOST_PASSWORD = 'password'
 
